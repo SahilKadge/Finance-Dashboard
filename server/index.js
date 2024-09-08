d8fe878fb3eb64d5c38dff2072d78a4e9f35b6c7
SahilKadge <sahilkadgeoffical@gmail.com>
2024-09-08 12:19:10 +0530

    upgraded moongose

--- server/index.js
+++ server/index.js
@@ -22,7 +22,9 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+  // origin: process.env.CLIENT_URL
+}));
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
